2bb49813d8dd1082983b50ee1e167cfea17026e1
JHLeeeMe <lejung92@gmail.com>
2022-12-30 18:32:36 +0900

    [Update] clean code & ```transform()``` 수정
    	수정함:        src/java/LogSplitter.java
    	수정함:        src/java/Transformer.java
    	수정함:        src/java/TransformerTopology.java

--- src/java/Transformer.java
+++ src/java/Transformer.java
@@ -1,22 +1,17 @@
 import java.util.Properties;
 
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 
 public class Transformer {
     public static void main(String[] args) {
-        Properties config = new Properties();
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-single-node:9092");
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "transformer");
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        TransformerConfig transformerConfig = new TransformerConfig();
+        Properties props = transformerConfig.getConfig();
 
         TransformerTopology transformerTopology = new TransformerTopology();
         Topology topology = transformerTopology.createTopology("raw", "transformed") ;
 
-        KafkaStreams streams = new KafkaStreams(topology, config);
+        KafkaStreams streams = new KafkaStreams(topology, props);
         streams.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
