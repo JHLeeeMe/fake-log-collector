933e586fa0a0ca1280a104dca71be850df40149e
JHLeeeMe <lejung92@gmail.com>
2023-01-11 18:12:25 +0900

    [Add] ```Message Queue Sender```(System V ipc) 구현
    	새 파일:       src/cpp/consumer-cpp/include/mqueue_sender.h
    	새 파일:       src/cpp/consumer-cpp/mqueue_sender.cpp

---
+++ src/cpp/consumer-cpp/mqueue_sender.cpp
@@ -0,0 +1,64 @@
+#include "mqueue_sender.h"
+
+MQSender::MQSender()
+    : _key(ftok("/dev/mqueue/", 255))
+{
+    _msg_id = msgget(_key, IPC_CREAT | 0644);
+    if (_msg_id < 0)
+    {
+        std::cerr << "msgget() failed..." << std::endl;
+        exit(1);
+    }
+}
+
+MQSender::MQSender(std::string& path)
+    : _key(ftok(path.c_str(), 255))
+{
+    _msg_id = msgget(_key, IPC_CREAT | 0644);
+    if (_msg_id < 0)
+    {
+        std::cerr << "msgget() failed..." << std::endl;
+        exit(1);
+    }
+}
+
+MQSender::~MQSender()
+{
+    if (msgget(_msg_id, 0) < 0)
+    {
+        return;
+    }
+
+    if (msgctl(_msg_id, IPC_RMID, nullptr) < 0)
+    {
+        std::cerr << "msgctl(..., IPC_RMID, ...) failed..." << std::endl;
+        exit(2);
+    }
+}
+
+void MQSender::send_msg(const char* value)
+{
+    std::string s{value};
+
+    memset(&_payload, 0x00, sizeof(struct Data));
+    strcpy(_payload.data, value);
+    memset(&_msg, 0x00, sizeof(struct MsgBuf));
+
+    std::string key = s.substr(0, s.find(','));
+    if (key == "nginx" || key == "apache")
+    {
+        _msg.type = 1;
+        _msg.payload = _payload;
+    }
+    else if (key == "flask")
+    {
+        _msg.type = 2;
+    }
+    _msg.payload = _payload;
+
+    if (msgsnd(_msg_id, &_msg, sizeof(struct MsgBuf), 0) < 0)
+    {
+        std::cerr << "msgsnd() failed..." << std::endl;
+        exit(2);
+    }
+}
