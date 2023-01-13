b128502f5bc553834deef61a3b458446cd07262b
JHLeeeMe <lejung92@gmail.com>
2023-01-13 15:31:24 +0900

    [Update] ```alter_retention_policy(...)``` 수정 & main funtion error handling
    	수정함:        src/python/log-generators/configs.py
    	수정함:        src/python/log-generators/log_generator.py

--- src/python/log-generators/configs.py
+++ src/python/log-generators/configs.py
@@ -5,9 +5,9 @@ def create_topic(topic: str, num_partitions: int = 3, replica: int = 1):
     admin_client = KafkaAdminClient(bootstrap_servers=['kafka-single-node:9092'])
 
     topic_config = {
-            'retention.ms': 1 * 3600 * 1000,  # 1 hour
+            'retention.ms': 1 * 3600 * 1000,                # 1 hour
             'delete.retention.ms': int(0.5 * 3600 * 1000),  # 0.5 hour
-            'retention.bytes': 5 * 1024 * 1024  # 5GB
+            'retention.bytes': 5 * 1024 * 1024              # 5GB
     }
     new_topic = NewTopic(name=topic,
                          num_partitions=num_partitions,
@@ -28,16 +28,20 @@ def describe_configs(topic: str):
     admin_client.close()
 
 
-def alter_retention_policy(topic: str, hour: int = 1):
-    assert (hour >= 1)
+def alter_retention_policy(topic: str, hour: float = 0.5):
+    assert (hour > 0)
 
     admin_client = KafkaAdminClient(bootstrap_servers=['kafka-single-node:9092'])
     assert (topic in admin_client.list_topics())
 
-    delete_retention_ms = hour * 3600 * 1000  # defualt 1 hour
+    retention_ms = hour * 3600 * 1000                    # 1 hour
+    delete_retention_ms = int((hour / 2) * 3600 * 1000)  # defualt 0.5 hour
+    retention_bytes = 5 * 1024 * 1024                    # 5GB
 
     resource = ConfigResource(resource_type=ConfigResourceType.TOPIC, name=topic)
-    resource.configs = {'delete.retention.ms': str(delete_retention_ms)}
+    resource.configs = {'retention.ms': str(retention_ms),
+                        'delete.retention.ms': str(delete_retention_ms),
+                        'retention.bytes': str(retention_bytes)}
 
     admin_client.alter_configs(config_resources=[resource])
     admin_client.close()
