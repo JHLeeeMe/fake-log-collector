31f92321a20ef715013efbe4682c244328083830
JHLeeeMe <lejung92@gmail.com>
2023-01-23 05:36:15 +0900

    [Update] public: ```date_str(...)``` 추가
    	수정함:        date_sync.cpp
    	수정함:        include/date_sync.hpp

--- src/cpp/save-to-hdfs/include/date_sync.hpp
+++ src/cpp/save-to-hdfs/include/date_sync.hpp
@@ -15,8 +15,9 @@ public:
 
     void        set_date(const std::string& key, const std::time_t& t);
     std::time_t get_date(const std::string& key);
+    void        date_str(char* buf, size_t buf_size, const std::string& key);
 private:
-    void init_date(std::time_t* t);
+    void init_date_map(std::time_t* t);
 private:
     std::map<std::string, std::time_t> _date_map;
 };
