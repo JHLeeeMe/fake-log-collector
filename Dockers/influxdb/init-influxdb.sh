9959a5770039e8580266d23b213b45bec816d228
JHLeeeMe <lejung92@gmail.com>
2023-02-06 20:01:57 +0900

    [Update] dashboards provisioning (미완)
    	새 파일:       Dockers/grafana/provisioning/dashboards/Container_Metrics.json
    	새 파일:       Dockers/grafana/provisioning/dashboards/Fake_Logs.json
    	새 파일:       Dockers/grafana/provisioning/datasources/all.yml
    	수정함:        Dockers/influxdb/bootstrap.sh
    	수정함:        Dockers/influxdb/init-influxdb.sh
    	수정함:        docker-compose.yml

--- Dockers/influxdb/init-influxdb.sh
+++ Dockers/influxdb/init-influxdb.sh
@@ -19,111 +19,108 @@ else
     META_DIR="$INFLUXDB_META_DIR"
 fi
 
-if ( [ ! -z "$INIT_USERS" ] || [ ! -z "$INFLUXDB_DB" ] || [ "$(ls -A /docker-entrypoint-initdb.d 2> /dev/null)" ] ) && [ ! "$(ls -d "$META_DIR" 2>/dev/null)" ]; then
+INIT_QUERY=""
+CREATE_DB_QUERY="CREATE DATABASE $INFLUXDB_DB"
 
-    INIT_QUERY=""
-    CREATE_DB_QUERY="CREATE DATABASE $INFLUXDB_DB"
+if [ ! -z "$INIT_USERS" ]; then
 
-    if [ ! -z "$INIT_USERS" ]; then
-
-        if [ -z "$INFLUXDB_ADMIN_PASSWORD" ]; then
-            INFLUXDB_ADMIN_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
-            echo "INFLUXDB_ADMIN_PASSWORD:$INFLUXDB_ADMIN_PASSWORD"
-        fi
-
-        INIT_QUERY="CREATE USER \"$INFLUXDB_ADMIN_USER\" WITH PASSWORD '$INFLUXDB_ADMIN_PASSWORD' WITH ALL PRIVILEGES"
-    elif [ ! -z "$INFLUXDB_DB" ]; then
-        INIT_QUERY="$CREATE_DB_QUERY"
-    else
-        INIT_QUERY="SHOW DATABASES"
+    if [ -z "$INFLUXDB_ADMIN_PASSWORD" ]; then
+        INFLUXDB_ADMIN_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
+        echo "INFLUXDB_ADMIN_PASSWORD:$INFLUXDB_ADMIN_PASSWORD"
     fi
 
-    INFLUXDB_INIT_PORT="8086"
+    INIT_QUERY="CREATE USER \"$INFLUXDB_ADMIN_USER\" WITH PASSWORD '$INFLUXDB_ADMIN_PASSWORD' WITH ALL PRIVILEGES"
+elif [ ! -z "$INFLUXDB_DB" ]; then
+    INIT_QUERY="$CREATE_DB_QUERY"
+else
+    INIT_QUERY="SHOW DATABASES"
+fi
 
-    INFLUXDB_HTTP_BIND_ADDRESS=127.0.0.1:$INFLUXDB_INIT_PORT INFLUXDB_HTTP_HTTPS_ENABLED=false influxd "$@" &
-    pid="$!"
+INFLUXDB_INIT_PORT="8086"
 
-    INFLUX_CMD="influx -host 127.0.0.1 -port $INFLUXDB_INIT_PORT -execute "
+INFLUXDB_HTTP_BIND_ADDRESS=127.0.0.1:$INFLUXDB_INIT_PORT INFLUXDB_HTTP_HTTPS_ENABLED=false influxd "$@" &
+pid="$!"
 
-    for i in {30..0}; do
-        if $INFLUX_CMD "$INIT_QUERY" &> /dev/null; then
-            /bin/bash bootstrap.sh
-            break
-        fi
-        echo 'influxdb init process in progress...'
-        sleep 1
-    done
+INFLUX_CMD="influx -host 127.0.0.1 -port $INFLUXDB_INIT_PORT -execute "
 
-    if [ "$i" = 0 ]; then
-        echo >&2 'influxdb init process failed.'
-        exit 1
+for i in {30..0}; do
+    if $INFLUX_CMD "$INIT_QUERY" &> /dev/null; then
+        /bin/bash bootstrap.sh
+        break
+    fi
+    echo 'influxdb init process in progress...'
+    sleep 1
+done
+
+if [ "$i" = 0 ]; then
+    echo >&2 'influxdb init process failed.'
+    exit 1
+fi
+
+if [ ! -z "$INIT_USERS" ]; then
+
+    INFLUX_CMD="influx -host 127.0.0.1 -port $INFLUXDB_INIT_PORT -username ${INFLUXDB_ADMIN_USER} -password ${INFLUXDB_ADMIN_PASSWORD} -execute "
+
+    if [ ! -z "$INFLUXDB_DB" ]; then
+        /bin/bash bootstrap.sh
+        #$INFLUX_CMD "$CREATE_DB_QUERY"
     fi
 
-    if [ ! -z "$INIT_USERS" ]; then
+    if [ ! -z "$INFLUXDB_USER" ] && [ -z "$INFLUXDB_USER_PASSWORD" ]; then
+        INFLUXDB_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
+        echo "INFLUXDB_USER_PASSWORD:$INFLUXDB_USER_PASSWORD"
+    fi
 
-        INFLUX_CMD="influx -host 127.0.0.1 -port $INFLUXDB_INIT_PORT -username ${INFLUXDB_ADMIN_USER} -password ${INFLUXDB_ADMIN_PASSWORD} -execute "
+    if [ ! -z "$INFLUXDB_USER" ]; then
+        $INFLUX_CMD "CREATE USER \"$INFLUXDB_USER\" WITH PASSWORD '$INFLUXDB_USER_PASSWORD'"
+
+        $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_USER\""
 
         if [ ! -z "$INFLUXDB_DB" ]; then
-            $INFLUX_CMD "$CREATE_DB_QUERY"
+            $INFLUX_CMD "GRANT ALL ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_USER\""
         fi
-
-        if [ ! -z "$INFLUXDB_USER" ] && [ -z "$INFLUXDB_USER_PASSWORD" ]; then
-            INFLUXDB_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
-            echo "INFLUXDB_USER_PASSWORD:$INFLUXDB_USER_PASSWORD"
-        fi
-
-        if [ ! -z "$INFLUXDB_USER" ]; then
-            $INFLUX_CMD "CREATE USER \"$INFLUXDB_USER\" WITH PASSWORD '$INFLUXDB_USER_PASSWORD'"
-
-            $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_USER\""
-
-            if [ ! -z "$INFLUXDB_DB" ]; then
-                $INFLUX_CMD "GRANT ALL ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_USER\""
-            fi
-        fi
-
-        if [ ! -z "$INFLUXDB_WRITE_USER" ] && [ -z "$INFLUXDB_WRITE_USER_PASSWORD" ]; then
-            INFLUXDB_WRITE_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
-            echo "INFLUXDB_WRITE_USER_PASSWORD:$INFLUXDB_WRITE_USER_PASSWORD"
-        fi
-
-        if [ ! -z "$INFLUXDB_WRITE_USER" ]; then
-            $INFLUX_CMD "CREATE USER \"$INFLUXDB_WRITE_USER\" WITH PASSWORD '$INFLUXDB_WRITE_USER_PASSWORD'"
-            $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_WRITE_USER\""
-
-            if [ ! -z "$INFLUXDB_DB" ]; then
-                $INFLUX_CMD "GRANT WRITE ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_WRITE_USER\""
-            fi
-        fi
-
-        if [ ! -z "$INFLUXDB_READ_USER" ] && [ -z "$INFLUXDB_READ_USER_PASSWORD" ]; then
-            INFLUXDB_READ_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
-            echo "INFLUXDB_READ_USER_PASSWORD:$INFLUXDB_READ_USER_PASSWORD"
-        fi
-
-        if [ ! -z "$INFLUXDB_READ_USER" ]; then
-            $INFLUX_CMD "CREATE USER \"$INFLUXDB_READ_USER\" WITH PASSWORD '$INFLUXDB_READ_USER_PASSWORD'"
-            $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_READ_USER\""
-
-            if [ ! -z "$INFLUXDB_DB" ]; then
-                $INFLUX_CMD "GRANT READ ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_READ_USER\""
-            fi
-        fi
-
     fi
 
-    for f in /docker-entrypoint-initdb.d/*; do
-        case "$f" in
-            *.sh)     echo "$0: running $f"; . "$f" ;;
-            *.iql)    echo "$0: running $f"; $INFLUX_CMD "$(cat ""$f"")"; echo ;;
-            *)        echo "$0: ignoring $f" ;;
-        esac
-        echo
-    done
-
-    if ! kill -s TERM "$pid" || ! wait "$pid"; then
-        echo >&2 'influxdb init process failed. (Could not stop influxdb)'
-        exit 1
+    if [ ! -z "$INFLUXDB_WRITE_USER" ] && [ -z "$INFLUXDB_WRITE_USER_PASSWORD" ]; then
+        INFLUXDB_WRITE_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
+        echo "INFLUXDB_WRITE_USER_PASSWORD:$INFLUXDB_WRITE_USER_PASSWORD"
     fi
 
+    if [ ! -z "$INFLUXDB_WRITE_USER" ]; then
+        $INFLUX_CMD "CREATE USER \"$INFLUXDB_WRITE_USER\" WITH PASSWORD '$INFLUXDB_WRITE_USER_PASSWORD'"
+        $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_WRITE_USER\""
+
+        if [ ! -z "$INFLUXDB_DB" ]; then
+            $INFLUX_CMD "GRANT WRITE ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_WRITE_USER\""
+        fi
+    fi
+
+    if [ ! -z "$INFLUXDB_READ_USER" ] && [ -z "$INFLUXDB_READ_USER_PASSWORD" ]; then
+        INFLUXDB_READ_USER_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
+        echo "INFLUXDB_READ_USER_PASSWORD:$INFLUXDB_READ_USER_PASSWORD"
+    fi
+
+    if [ ! -z "$INFLUXDB_READ_USER" ]; then
+        $INFLUX_CMD "CREATE USER \"$INFLUXDB_READ_USER\" WITH PASSWORD '$INFLUXDB_READ_USER_PASSWORD'"
+        $INFLUX_CMD "REVOKE ALL PRIVILEGES FROM \"$INFLUXDB_READ_USER\""
+
+        if [ ! -z "$INFLUXDB_DB" ]; then
+            $INFLUX_CMD "GRANT READ ON \"$INFLUXDB_DB\" TO \"$INFLUXDB_READ_USER\""
+        fi
+    fi
+
+fi
+
+for f in /docker-entrypoint-initdb.d/*; do
+    case "$f" in
+        *.sh)     echo "$0: running $f"; . "$f" ;;
+        *.iql)    echo "$0: running $f"; $INFLUX_CMD "$(cat ""$f"")"; echo ;;
+        *)        echo "$0: ignoring $f" ;;
+    esac
+    echo
+done
+
+if ! kill -s TERM "$pid" || ! wait "$pid"; then
+    echo >&2 'influxdb init process failed. (Could not stop influxdb)'
+    exit 1
 fi
